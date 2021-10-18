fa50e1ab9497aa95134b3cd6ab071454a48fdb96
Mateus Leonardo <m4teusmatos@gmail.com>
2021-10-18 11:57:19 -0300

    creating states

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const segundos, setSegundos = useState(0)
+  const minutos, setMinutos = useState(0)
+
+  const alarmeSound, setAlarmeSound = useState([
+    {
+      selecionado: true,
+      som: "alarme 1",
+      file: "alarme1.mp3"
+    },
+    {
+      selecionado: false,
+      som: "alarme 2",
+      file: "alarme2.mp3"
+    }
+  ])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +33,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'rgb(70, 130, 219)'
   },
 });
