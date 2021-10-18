a85291535f4d9c0f3efd6b989421dc303e8fc9fe
Mateus Leonardo <m4teusmatos@gmail.com>
2021-10-18 13:51:14 -0300

    styling button

--- components/Buttons.js
+++ components/Buttons.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-import { TouchableOpacity, View, Text } from "react-native";
+import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
 
 export default props =>{
     return(
         <View>
-            <TouchableOpacity><Text>Alarme 1</Text></TouchableOpacity>
+            <TouchableOpacity style={style.btnSelect}><Text>Alarme 1</Text></TouchableOpacity>
         </View>
     )
 }
+
+const style = StyleSheet.create({
+    btnSelect: {
+        padding: 8,
+        backgroundColor: '#c9e4f0',
+        borderRadius: 10
+    }
+})
+
